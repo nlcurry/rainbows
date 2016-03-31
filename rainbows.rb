702184d4b94344f0b4b9cd869413c7b6996aacf2
Nadine <nlcurry@gmail.com>
2016-03-30 23:41:25 -0700

    homework

--- rainbows.rb
+++ rainbows.rb
@@ -1,3 +1,5 @@
+require "rainbow"
+
 red = [255,0,0]
 green = [0,255,0]
 blue = [0,0,255]
@@ -17,6 +19,14 @@ color_lookup = {
   [255,0,125] => "raspberry"
 }
 
+rainbow = [{:red => {r: 255, g: 0, b: 0}}, 
+          {:orange => {r: 255, g: 125, b: 0}}, 
+          {:yellow => {r: 255, g: 255, b: 0}}, 
+          {:green => {r: 0, g: 255, b: 0}}, 
+          {:blue => {r: 0, g: 0, b: 255}}, 
+          {:indigo => {r: 75, g: 0, b: 130}}, 
+          {:violet => {r: 125, g: 0, b: 255}}]
+
 def mix_colors(color1,color2)
   mixed_color = []
   3.times do |i|
@@ -27,3 +37,8 @@ end
 
 new_color = mix_colors(red,green)
 puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lookup[new_color]} #{new_color}"
+
+rainbow.length.times do |element|
+   color_choice = rainbow[element].keys.join
+   puts Rainbow("#{color_choice}").color(color_choice.to_sym)
+end
